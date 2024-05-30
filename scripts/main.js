ac667a496c1a23e57de1142057dcf96af6ff9f3c
David Lopez <48055910+daload@users.noreply.github.com>
2024-05-30 12:50:41 +0200

    Fix 7

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,3 @@ AOS.init({
 });
 
 // Add your javascript here
-function getAge() {
-  var today = new Date()
-  var birthDate = new Date(2000, 6, 21)
-  var age = today.getFullYear() - birthDate.getFullYear()
-  var m = today.getMonth() - birthDate.getMonth()
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--
-  }
-  return age
-}
